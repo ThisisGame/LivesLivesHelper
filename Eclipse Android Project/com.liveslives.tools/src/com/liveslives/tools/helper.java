f4d81e0de43a7a04bba46dc419c6b4729b7d882b
captain <980097191@qq.com>
2017-03-09 14:26:33 +0800

    Add Judge File Exist In Assets

--- Eclipse Android Project/com.liveslives.tools/src/com/liveslives/tools/helper.java
+++ Eclipse Android Project/com.liveslives.tools/src/com/liveslives/tools/helper.java
@@ -1,8 +1,12 @@
 package com.liveslives.tools;
 
+import java.io.InputStream;
+import java.util.Hashtable;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
@@ -11,12 +15,15 @@ public class helper
 {
 	private static boolean isInit=false;
 	private static Activity sActivity = null;
+	private static AssetManager sAssetManager;
+	private static Hashtable mFileTable = new Hashtable();
 	
 	public static void init(Object paramObject)
 	{
 		if(!isInit)
 		{
 			sActivity = (Activity)paramObject;
+			sAssetManager = sActivity.getAssets();
 			isInit = true;
 			Log.i("liveslives", "init success");
 		}
@@ -62,5 +69,30 @@ public class helper
         	return "get mac faild";
         }
         return info.getMacAddress(); 
+	}
+	
+	//get file exist in apk assets
+	public static boolean ExistInDataPath(String paramString)
+	{
+		boolean bool = false;
+	    if (mFileTable.containsKey(paramString))
+	      return ((Boolean)mFileTable.get(paramString)).booleanValue();
+	    if (sAssetManager != null)
+	    {
+	      InputStream localInputStream = null;
+	      try
+	      {
+	        localInputStream = sAssetManager.open(paramString);
+	        bool = true;
+	        mFileTable.put(paramString, Boolean.valueOf(true));
+	        localInputStream.close();
+	      }
+	      catch (Exception localException)
+	      {
+	        mFileTable.put(paramString, Boolean.valueOf(false));
+	        Log.e("ReadFile exception", localException.toString());
+	      }
+	    }
+	    return bool;
 	}
 }
